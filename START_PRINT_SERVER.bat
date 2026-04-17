1843a2dafa5b46fb80ae8e33f37a14dc179e5a82
kensiccc <your-email@example.com>
2026-04-17 16:07:07 +0800

    first commit

--- START_PRINT_SERVER.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo =========================================
-echo   House Blend POS - PDF Print Spooler
-echo =========================================
-echo.
-node print-server.js
-pause
